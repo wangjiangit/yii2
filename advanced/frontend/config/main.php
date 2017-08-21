2a35c1bc914308c68da5da891fc49eed4374c0b1
wang jian <jiji_12342006@163.com>
2017-08-21 16:45:37 +0800

    添加 错误处理，日志处理，SESSION和cookie处理示例

--- advanced/frontend/config/main.php
+++ advanced/frontend/config/main.php
@@ -24,42 +24,63 @@ return [
             // this is the name of the session cookie used for login on the frontend
             'name' => 'advanced-frontend',
         ],
+       /* 'session'=>[
+          'class'=>'yii\web\DbSession',
+          'db'=>'db',
+          'sessionTable'=>'session'
+        ],*/
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
+            'flushInterval' => 1000,
             'targets' => [
-                [
+                'file1_log' => [
                     'class' => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
+                    // 'enabled'=>false, //或者通过Yii::$app->log->targets['file1_log']->enabled=false来禁用
+                    //'logVars'=>[],
+                    //  'exportInterval'=>1000
                 ],
+               /* 'db1_log'=>[
+                    'class'=>'yii\log\DbTarget',
+                    'levels'=>['error','warning'],
+                    'logVars'=>[],
+                    'db'=>'db',
+                    'logTable'=>'log',
+                    'exportInterval'=>1000
+                ]*/
             ],
         ],
         'errorHandler' => [
+          //  'maxSourceLines'=>19,
+         //   'displayVars'=>[],
+          //    'errorView'=>'@yii/views/errorHandler/error.php',//显示不包含函数调用栈信息的错误信息是使用， 当YII_DEBUG 为 false时，所有错误都使用该视图。
+            // 'exceptionView'=>@yii/views/errorHandler/exception.php, // 显示包含函数调用栈信息的错误信息时使用。
             'errorAction' => 'site/error',
         ],
 
-       /* 'urlManager' => [
-            'enablePrettyUrl' => false,
-            'showScriptName' => false,
-            'enableStrictParsing'=>fasle,
-            'suffix'=>'.html',
-            'rules' => [
-                'city/<id:\d+>'=>'city/index',
-                [
-                    'pattern'=>'citymap',
-                    'route'=>'city/index',
-                    'suffix'=>'.json'
-                ],
-                [
-                    'pattern'=>'city/<action:(index|list)>/<id:\d{4}>',
-              //      'pattern'=>'GET city/<action:(index|list)>/<id:\d{4}>', //模式前面有HTTP方法的，只对 RESTful APIs 有效
-                    'route'=>'city/<action>',
-                    'defaults'=>['id'=>1],
-                    'suffix'=>'.shtml'
-                ],
-                'http://www.5izan.com/login'=>'site/login',
-                'http://<language:\w+>.5izan.com'=>'site/<language>'
-            ],
-        ],*/
+        /* 'urlManager' => [
+             'enablePrettyUrl' => false,
+             'showScriptName' => false,
+             'enableStrictParsing'=>fasle,
+             'suffix'=>'.html',
+             'rules' => [
+                 'city/<id:\d+>'=>'city/index',
+                 [
+                     'pattern'=>'citymap',
+                     'route'=>'city/index',
+                     'suffix'=>'.json'
+                 ],
+                 [
+                     'pattern'=>'city/<action:(index|list)>/<id:\d{4}>',
+               //      'pattern'=>'GET city/<action:(index|list)>/<id:\d{4}>', //模式前面有HTTP方法的，只对 RESTful APIs 有效
+                     'route'=>'city/<action>',
+                     'defaults'=>['id'=>1],
+                     'suffix'=>'.shtml'
+                 ],
+                 'http://www.5izan.com/login'=>'site/login',
+                 'http://<language:\w+>.5izan.com'=>'site/<language>'
+             ],
+         ],*/
 
         /*  'assetManager'=>[
           'bundles'=>[
--- advanced/frontend/controllers/CityController.php
+++ advanced/frontend/controllers/CityController.php
@@ -4,22 +4,23 @@ namespace frontend\controllers;
 
 use Yii;
 use frontend\filters\TimeActionFilter;
+use yii\web\NotFoundHttpException;
 use yii\web\Request;
 use yii\imagine\Image;
 
 class CityController extends \yii\web\Controller
 {
 
-    public $layout='main';
+    public $layout = 'main';
 
 
     public function behaviors()
     {
         return [
             [
-          //      'class'=>'frontend\filters\TimeActionFilter',
-                'class'=>TimeActionFilter::className(),
-                'only'=>['index']
+                //      'class'=>'frontend\filters\TimeActionFilter',
+                'class' => TimeActionFilter::className(),
+                'only' => ['index']
             ]
 
         ];
@@ -28,8 +29,8 @@ class CityController extends \yii\web\Controller
     public function actions()
     {
         return [
-            'helloworld'=>'frontend\actions\HelloWorldAction',
-            'page'=>'yii\web\ViewAction'
+            'helloworld' => 'frontend\actions\HelloWorldAction',
+            'page' => 'yii\web\ViewAction'
         ];
     }
 
@@ -38,33 +39,81 @@ class CityController extends \yii\web\Controller
         //使用yiisoft/yii2-imagine
         // Image::frame('@runtime/image/a.jpg',5,'4FF',0)->save(Yii::getAlias('@runtime/image/b.jpg'),['jpeg_quality' => 50]);
         //  Image::text('@runtime/image/a.jpg','ABCD','@runtime/image/aicomoon.ttf',[50,50],['color'=>'FFF']);
-       //var_dump(Yii::$app->urlManager->enableStrictParsing);exit;
+        //var_dump(Yii::$app->urlManager->enableStrictParsing);exit;
         /*Yii::$app->response->statusCode=200;
         Yii::$app->response->headers->set('wj','hahhds');
         Yii::$app->response->format=\yii\web\Response::FORMAT_JSONP;
         Yii::$app->response->data=['data'=>['name'=>'wj','age'=>18],'callback'=>'nihao'];
         Yii::$app->response->send();*/
 
-     /*   return Yii::createObject([
-            'class'=>'yii\web\Response',
-            'statusCode'=>200,
-            'format'=>\yii\web\Response::FORMAT_JSON,
-            'data'=>[
-                'name'=>'wj',
-                'age'=>18
-            ]
-        ]);*/
+        /*   return Yii::createObject([
+               'class'=>'yii\web\Response',
+               'statusCode'=>200,
+               'format'=>\yii\web\Response::FORMAT_JSON,
+               'data'=>[
+                   'name'=>'wj',
+                   'age'=>18
+               ]
+           ]);*/
 
-       // $this->redirect('http://www.baidu.com')
+        // $this->redirect('http://www.baidu.com')
 
         //$this->asJson(['NAME'=>'json','X'=>'ss']);
-       /* Yii::$app->response->on(\yii\web\Response::EVENT_AFTER_SEND,function(){
-           Yii::trace('下载事件监听');
-        });
-        Yii::$app->response->sendFile(Yii::getAlias('@runtime/image/a.jpg'))->send();*/
+        /* Yii::$app->response->on(\yii\web\Response::EVENT_AFTER_SEND,function(){
+            Yii::trace('下载事件监听');
+         });
+         Yii::$app->response->sendFile(Yii::getAlias('@runtime/image/a.jpg'))->send();*/
+        /* Yii::beginProfile('profile1');
+         for($i=0;$i<10000;$i++){
+             echo 'asdf';
+         }
+         Yii::endProfile('profile1');*/
+        /* Yii::trace('trace1');
+         Yii::info('this is info message');
+         Yii::warning('warning');
+         Yii::error('error',__METHOD__);*/
+        // $a=10/0;
+        //  throw new NotFoundHttpException('亲,你要找的页面不存在哦');
+        /*
+                Yii::$app->session->open();
+               Yii::$app->session->set('name','wj');*/
+        //var_dump(Yii::$app->session->get('name'));exit;
+        /*Yii::$app->session->remove('name');
+        exit;*/
 
-        
-       return $this->render('index');
+        /* //一次性数据，访问之后即消失
+        $session = Yii::$app->session;
+        $session->setFlash('postDeleted', 'You have successfully deleted your post.');
+        $session->addFlash('postDeleted', 'You have successfully deleted your post2'); //addFlash会将前面同名的转化为数组，以追加
+        echo $session->getFlash('postDeleted');
+        // $result 为 false，因为flash信息已被自动删除
+        $result = $session->hasFlash('postDeleted');
+         */
+
+
+        //读取cookie
+        //  $cookies=Yii::$app->request->cookies; //yii\web\CookieCollection
+        //  var_dump($cookies->getValue('_csrf-frontend'));exit;
+        // echo $cookies->get('_csrf-frontend')->value;exit;
+        // echo $cookies['_csrf-frontend'];exit;
+        // var_dump($cookies->has('_csrf-frontend'));exit;
+
+
+        //发送cookie
+       $cookies = Yii::$app->response->cookies;
+        /*
+        $cookies->add(new \yii\web\Cookie([
+                    'name' => 'my_name',
+                    'value' => 'wj'
+                ]
+            )
+        );
+         $cookies->remove('my_name');
+        */
+
+
+
+        return $this->render('index');
 
     }
 
