7a94fd5c1937ec59681afc75ce5f2108ec15398f
wang jian <jiji_12342006@163.com>
2017-08-24 18:20:08 +0800

    编写HTML和URL辅具类示例

--- advanced/frontend/views/city/index.php
+++ advanced/frontend/views/city/index.php
@@ -35,6 +35,8 @@ $this->on(View::EVENT_AFTER_RENDER,function($event){
 <?= Url::to(['country/index','src'=>'web_1'],'http')?>
 <?= Url::home() ?>
 <?= Url::canonical()?>
+<?= Yii::getAlias('@webroot')?>
+<?php var_dump(Url::isRelative('/post/index'))  ?>
 
 
 <!-- HTML辅助类使用BEGIN -->
@@ -42,7 +44,62 @@ $this->on(View::EVENT_AFTER_RENDER,function($event){
 <?= Html::tag('p','中国',['data'=>['name'=>'wj','age'=>2]])?>
 <?= Html::tag('p','安庆',['data'=>['option'=>['web1'=>'web1_value','web2'=>'web2_value'],'scene'=>'web']])?>
 <?= Html::tag('p','安庆',['title'=>['option'=>'option1','scene'=>'web']])?>
-<?=  ?>
+
+<?php
+$options=['class'=>'btn btn-default','style'=>['width'=>'100px','height'=>'20px']];
+
+/*$type='success';
+
+if($type=='success'){
+    Html::removeCssClass($options,'btn-default');
+ //   Html::addCssClass($options,['btn-default','btn-lg']);
+ //   Html::addCssClass($options,'btn-success btn-lg');
+    Html::addCssClass($options,'btn-success');
+    Html::removeCssStyle($options,['height']);
+    Html::addCssStyle($options,"height: 50px; position: absolute;");
+}
+
+echo Html::tag('div','保存',$options);*/
+
+echo Html::encode('<div>"8"代表发财</div>');
+?>
+
+
+<?= Html::beginForm(['post/index','src'=>'web1'])?>
+<?=Html::button('按钮1',['class'=>'btn btn-default']) ?>
+<?=Html::resetButton('按钮2',['class'=>'btn btn-default'])?>
+<?= Html::submitButton('按钮3',['class'=>'btn btn-default'])?>
+<?= Html::endForm()?>
+
+<?= Html::input('text','username','zhangsan') ?>
+<?= Html::textInput('username1','lisi')?>
+<?= Html::radio('habby',true,['value'=>'zuqiu']) ?> 足球
+<?= Html::radio('habby',false,['value'=>'taiqiu']) ?>台球
+<?= Html::radioList('hobby1',12,[12=>'中国',13=>'美国']) ?>
+
+<?= Html::checkbox('options[]',true,['value'=>'value1']) ?>选项1
+<?= Html::checkbox('options[]',false,['value'=>'value1']) ?>选项2
+<?= Html::checkboxList('options1[]',[12,13],[12=>'中国',13=>'美国']) ?>
+
+<?= Html::dropDownList('city',13,[12=>'中国',13=>'美国'])?>
+<?= Html::listBox('city',1,[12=>'中国',13=>'美国'])?>
+
+<?= Html::label('label标签','female') ?>
+
+<?= Html::style('.danger {color:yellow}  .info {color:#ccc}') ?>
+<?= Html::script('console.log("Yii application")') ?>
+
+<?= Html::cssFile('@web/assets/ie5.css',['condition'=>'IE 5']);?>
+<?= Html::cssFile('@web/assets/css1.css');?>
+<?= Html::jsFile('@web/assets/js1.js') ?>
+
+<?= Html::a('国外',['post/index','id'=>3]);?>
+<?= Html::mailto('我的邮件','jiji_12342006@163.com') ?>
+<?= Html::img('@web/images/a.jpg')?>
+<?= Html::ul(['asdf'=>'one',2=>'two'])?>
+
+
+
 <!-- HTML辅助类使用END-->
 
 
