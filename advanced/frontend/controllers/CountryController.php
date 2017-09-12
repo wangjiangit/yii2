2e06405e577069558ed38c0e13ecff0cecaec4ea
wang jian <jiji_12342006@163.com>
2017-09-12 14:57:13 +0800

    数据提供器 ，数据小部件  ，操作客户端脚本 示例

--- advanced/frontend/controllers/CountryController.php
+++ advanced/frontend/controllers/CountryController.php
@@ -1,10 +1,13 @@
 <?php
-namespace  frontend\controllers;
+namespace frontend\controllers;
 
 use Yii;
 use yii\web\Controller;
 use yii\data\Pagination;
 use frontend\models\Country;
+use yii\data\Sort;
+use yii\data\ArrayDataProvider;
+use yii\data\ActiveDataProvider;
 
 class CountryController extends Controller
 {
@@ -12,6 +15,7 @@ class CountryController extends Controller
 
     public function actionIndex()
     {
+
         $query = Country::find();
 
         $pagination = new Pagination([
@@ -19,7 +23,66 @@ class CountryController extends Controller
             'totalCount' => $query->count(),
         ]);
 
-        $countries = $query->orderBy('name')
+        $sort = new Sort([
+            'attributes' => [
+                'name' => [
+                    'asc' => ['name' => SORT_ASC],
+                    'desc' => ['name' => SORT_DESC],
+                    'default' => SORT_DESC,
+                    'label' => 'Name'
+                ]
+            ],
+            'defaultOrder' => [
+                'name' => SORT_DESC
+            ],
+            'sortParam' => 'sort'
+        ]);
+        //echo $sort->createUrl('name');
+       // echo $sort->link('name') ;exit;
+        //  $countries = $query->orderBy('name')
+
+        //数据提供者示例
+        /*$data = [
+            ['id' => 1, 'name' => 'name 1'],
+            ['id' => 2, 'name' => 'name 2'],
+            ['id' => 100, 'name' => 'name 100'],
+        ];
+
+        $provider = new ArrayDataProvider([
+            'allModels' => $data,
+            'key'=>function($model){
+                return md5($model['id']);
+            },
+            'pagination' => [
+                'pageSize' => 10,
+            ],
+            'sort' => [
+                'attributes' => ['id', 'name'],
+                'defaultOrder'=>['id'=>SORT_DESC]
+            ],
+        ]);
+
+        // 获取当前请求页的每一行数据
+        $rows = $provider->getModels();
+        $keys= $provider->getKeys();
+        $count=$provider->getTotalCount();
+        var_dump($rows);exit;*/
+
+          /* //使用 ActiveDataProvider 数据提供者
+             $provider = new ActiveDataProvider([
+                   'query' => $query,
+                   'pagination' => [
+                       'pageSize' => 10,
+                   ],
+                   'sort' => [
+                       'defaultOrder' => [
+                           'code' => SORT_ASC,
+                       ]
+                   ],
+               ]);*/
+
+
+        $countries = $query->orderBy($sort->orders)
             ->offset($pagination->offset)
             ->limit($pagination->limit)
             ->all();
@@ -27,12 +90,9 @@ class CountryController extends Controller
         return $this->render('index', [
             'countries' => $countries,
             'pagination' => $pagination,
+    //        'provider'=>$provider
         ]);
     }
-
-
-
-
 
 
 }
--- advanced/frontend/views/country/index.php
+++ advanced/frontend/views/country/index.php
@@ -1,6 +1,11 @@
 <?php
 use yii\helpers\Html;
 use yii\widgets\LinkPager;
+use yii\grid\GridView;
+
+/**
+ * @var $this \yii\web\View
+ */
 ?>
     <h1>Countries</h1>
     <ul>
@@ -13,3 +18,16 @@ use yii\widgets\LinkPager;
     </ul>
 
 <?= LinkPager::widget(['pagination' => $pagination]) ?>
+
+
+<?php /* echo GridView::widget([
+    'dataProvider'=>$provider
+]) ;*/?>
+
+<?php
+
+ $this->registerJsFile('js/js1.js',['position'=>$this::POS_HEAD],'KEY1');
+$this->registerJs('var a =3;',$this::POS_END);
+//$this->registerJsFile('http://example.com/js/main.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+
+?>
